1988919ba6ffc0f1776a36b2c7503e93364e3fe2
Farizal <123220199@student.upnyk.ac.id>
2025-05-25 22:03:24 +0700

    Update 2

--- routes/NotesRoute.js
+++ routes/NotesRoute.js
@@ -2,10 +2,11 @@ import express from "express";
 import { getNotes, createNotes, updateNotes, deleteNotes, getNotesById } from "../controllers/NotesController.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
-
 const router = express.Router();
 
-router.get("/notes", getNotes);
+router.get('/token', refreshToken);
+
+router.get("/notes", verifyToken, getNotes);
 router.get("/notes/:id", getNotesById);
 router.post("/add-notes", createNotes);
 router.put("/edit-notes/:id", updateNotes);
